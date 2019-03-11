585424df75de1faf080bd35386afcb3b87f14e5c
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-03-11 17:27:59 +0100

    Updated test to use the plot.ae function

--- temp_tests/gorepatti_test.R
+++ temp_tests/gorepatti_test.R
@@ -19,24 +19,13 @@ overlap.set <- format_to_ref(gorrepati.expr_data, interp.oud$interpol.gene_expr)
 age.est <- estimate.worm_age(samp = overlap.set$samp, 
                              refdata = overlap.set$refdata, 
                              ref.time_series = interp.oud$time.series,
-                             est.time = 27)
+                             time.sd = 7, bootstrap.n = 50,
+                             est.time = 27, bootstrap.time_window = 5)
 
 age.est$age.estimates
+plot(age.est)
 
-
-par(mfrow=c(4,2), mar=c(4,3,3,1))
-pb <- sapply(1:ncol(age.est$age.estimates), function(i){
-  
-  plot(interp.oud$time.series, age.est$cors[,i], type = 'l', lwd=2,
-       main=colnames(age.est$cors)[i], xlab = 'time', ylab='corr.score')
-  
-  ae <- t(age.est$age.estimates[c(1,2),i])
-  points(ae, pch='|', cex=2, col='firebrick')
-  
-  text(ae, pos=1, 
-       labels = paste(round(ae[1], 2), 'h', sep=''),
-       offset = 1)
-})
+plot(age.est$init.est.times, age.est$age.estimates[,1])
 
 
 
