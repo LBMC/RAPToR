789a7ee1502a2dae64c2b326c58978934906b4d5
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-04-08 15:31:30 +0200

    Added reference timetables for developmental stages

---
+++ R/ref_tables-data.R
@@ -0,0 +1,20 @@
+#' Reference time tables
+#'
+#' A list of reference tables giving the approximate timings of key developmental 
+#' stages in the reference datasets.
+#'
+#' @docType data
+#'
+#' @usage data(ref_tables)
+#'
+#' @format a list of dataframes for the reference datasets with the developmental stage and its time.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source The timings for developmental stages are taken from the datasets' papers.
+#'
+#' @examples
+#' data(ref_tables)
+#' head(ref_tables)
+"ref_tables"
