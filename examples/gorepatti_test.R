1529038738c48e60ff1f32e65024d1d86dccc0ae
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-04-09 09:31:24 +0200

    Updated example

--- examples/gorepatti_test.R
+++ examples/gorepatti_test.R
@@ -5,17 +5,17 @@ library(wormAge)
 gorrepati.expr_data <- GPL200_to_WB("GSE52747")
 
 gorrepati.pheno <- read.table(textConnection(
-  "id 	batch 	strain
-  GSM2092609 	1 	N2
-  GSM2092610 	1 	N2
-  GSM2092611 	1 	N2
-  GSM2092612 	2 	Huis1
-  GSM2092613 	2 	DeltaNTPOP1
-  GSM2092614 	2 	Huis1
-  GSM2092615 	2 	DeltaNTPOP1
-  GSM2092616 	2 	Huis1
-  GSM2092617 	2 	DeltaNTPOP1
-  "), sep='\t', h=T, row.names=1)
+"id 	batch 	strain
+GSM2092609 	1 	N2
+GSM2092610 	1 	N2
+GSM2092611 	1 	N2
+GSM2092612 	2 	Huis1
+GSM2092613 	2 	DeltaNTPOP1
+GSM2092614 	2 	Huis1
+GSM2092615 	2 	DeltaNTPOP1
+GSM2092616 	2 	Huis1
+GSM2092617 	2 	DeltaNTPOP1
+"), sep='\t', h=T, row.names=1)
 gorrepati.pheno <- gorrepati.pheno[colnames(gorrepati.expr_data),]
 
 guess <- 26 # the paper states the worms were grown for 26 hrs at 20C
@@ -35,29 +35,29 @@ age.est <- estimate.worm_age(samp = gorrepati.expr_data,
 
 age.est$age.estimates
 
+
+
 # plot correlation profiles
 par(mfrow=c(3,2))
-plot(age.est, show.init_estimate = T)
+plot_cor.ae(age.est, show.init_estimate = T)
 
 
+# plot age estimates
+par(mfrow=c(1,1))
+plot(age.est, col.i=4)
 
 
 # plot age vs pheno data
-p <- par(mfrow=c(1,2), mar=c(7,4,3,1))
-boxplot(age.est$age.estimates[,1]~gorrepati.pheno$strain,
-        main="Estimated age per strain", las=2)
-require(beeswarm)
-beeswarm(age.est$age.estimates[,1]~gorrepati.pheno$strain, 
-         col = 2, pch=16, cex=1.5, lwd=2, add=T)
+par(mfrow=c(2,1))
 
+plot(age.est, groups = gorrepati.pheno$strain, main="By strain",
+     col.i=4, col= as.numeric(gorrepati.pheno$strain))
 
-boxplot(age.est$age.estimates[,1]~gorrepati.pheno$batch, 
-        main="Estimated age per batch")
-beeswarm(age.est$age.estimates[,1]~gorrepati.pheno$batch, 
-         col = 2, pch=16, cex=1.5, lwd=2, add=T)
-par(p)
+plot(age.est, groups = gorrepati.pheno$batch, main="By batch",
+     col.i=4, col=gorrepati.pheno$batch)
 
 
 # print time window
 diff(range(age.est$age.estimates[,1]))
+
 
