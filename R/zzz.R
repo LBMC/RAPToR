f50d8c0a94e75a9f0cd580f80ff468fe0b42cc04
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-06-17 17:55:46 +0200

    Removed the YA_adult2 reference dataset

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
   if(getRversion() >= "2.15.1"){
     
     utils::globalVariables(c("Cel_embryo", "Cel_larval",  "Cel_YA_adult1", 
-                             "Cel_YA_adult2", "ref_tables"))
+                             #"Cel_YA_adult2", 
+                             "ref_tables"))
   }
   
 }
