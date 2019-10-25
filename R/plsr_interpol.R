e69799947f9e7277a737d2394fb316e8b5665aa0
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-10-25 10:07:25 +0200

    Updated doc and param names

--- R/plsr_interpol.R
+++ R/plsr_interpol.R
@@ -4,10 +4,10 @@
 #' This is done with a multi-target PLSR model
 #' 
 #' @param X gene expression matrix of reference time series, genes as rows, (ordered) individuals as columns
-#' @param n.inter number of timepoints to return in interpolated data
 #' @param time.series timepoints of the reference (X).
+#' @param df the df parameter passed on to the \code{\link[splines]{ns}}
+#' @param n.inter number of timepoints to return in interpolated data, defaults to 200.
 #' @param t.min,t.max defaults to min and max of \code{time.series} ; start and end times of new time series ; ignored if new.timepoints is given.
-#' @param new.timepoints vector of length n.inter with the new time series (overrides t.min and t.max)
 #' @param return.model if TRUE, returns the PLSR model object
 #' 
 #' @export
@@ -17,21 +17,6 @@
 #' \donttest{
 #' data(Cel_larval)
 #' par(mfrow=c(2,2))
-#'
-#' interpold <- interpol_refdata(X = Cel_larval$X, n.inter = 200, 
-#'                               time.series = Cel_larval$time.series, ,
-#'                               ica.nc = 10, keep.c = 1:10,
-#'                               plot = TRUE)
-#'
-#' pb <- sapply(c(2,5,13,50), function(i){
-#'    plot(Cel_larval$time.series, Cel_larval$X[i,],
-#'         type = 'l', lwd=2, 
-#'         main=rownames(Cel_larval$X)[i], xlab = 'time')
-#'    points(interpold$time.series, interpold$interpol.gene_expr[i,],
-#'           type = 'l', lwd=2, col='firebrick')
-#'   if(i==5){
-#'     legend('topright', legend = c("Initial ref. data", "Interpolated data"),
-#'            lwd=3, col=c('black', 'firebrick'), bty = "n")
 #'   }
 #' })
 #' }
@@ -42,14 +27,18 @@
 #' @importFrom pls plsr RMSEP
 
 plsr_interpol <- function(X, time.series, df, 
-                          n.inter=100, scale=T, 
-                          knots = NULL)
+                          n.inter = 200, 
+                          tmin = min(time.series), tmax = max(time.series),
+                          scale = T, knots = NULL)
 {
   if(n.inter<ncol(X)){
-    stop("n.inter must be larger than ncol(X)")
+    warning("n.inter should be larger than ncol(X)")
   }
   if(length(time.series)!=ncol(X)){
     stop("time series must be of length ncol(X)")
+  }
+  if(tmin < min(time.series) | tmax > max(time.series)){
+    stop("tmin and tmax must be within time.series")
   }
 
   
@@ -60,15 +49,16 @@ plsr_interpol <- function(X, time.series, df,
   m.X <- pls::plsr(t(X)~t.mat, scale=scale, validation='CV') # apply plsr model
   
   # get appropriate ncomp for plsr with built-in CV
-  cv <- RMSEP(m.X)$val['CV',,]
-  nc <- which.min(colMeans(cv)) -1
+  cv <- pls::RMSEP(m.X)$val['CV',,]
+  nc <- which.min(colMeans(cv)) - 1
   if(nc==0)
     nc <- which.min(colMeans(cv)[-1])
   
   # Build interpolation
-  inter <- seq(min(time.series), max(time.series), length.out = n.inter)
+  inter <- seq(tmin, tmax, length.out = n.inter)
+  
   inter.mat <- stats::predict(t.mat, inter) # make interpolated ns time series 
-  pred.mat <- stats::predict(m.X, newdata = inter.mat, comps=1:nc)
+  pred.mat <- stats::predict(m.X, newdata = inter.mat, comps=1:nc) # use model to predict gene expr
   
   return(list(plsr.model=m.X,    # PLSR model
               ts=inter,          # time series (interpolated)
