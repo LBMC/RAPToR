6dcf501465ce4a9c165388e9756bcb11591cb438
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2019-12-13 17:44:54 +0100

    Removed old dataset names from zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,8 @@
 .onLoad <- function(libname = find.package("wormAge"), pkgname = "wormAge"){
   # CRAN Note avoidance
   if(getRversion() >= "2.15.1"){
-    
-    utils::globalVariables(c("Cel_embryo", "Cel_larval",  "Cel_YA_adult1", 
-                             #"Cel_YA_adult2", 
-                             "ref_tables"))
+    # 
+    utils::globalVariables(c("ref_table"))
   }
   
 }
