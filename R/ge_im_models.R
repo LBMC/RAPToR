d94668bf6b2494ae90ed592a81a28192dcc57f5c
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-02-21 10:36:17 +0100

    Added the hidden function for gam ica model

--- R/ge_im_models.R
+++ R/ge_im_models.R
@@ -29,3 +29,36 @@
     return(tcrossprod(m$pca$x, preds))
   else return(preds)
 }
+
+
+
+#' @importFrom stats as.formula update 
+#' @import mgcv
+.model_gam_ica <- function(X, p, formula, nc = ncol(X), center = T, drX = F){
+  if(drX){
+    ica <- X
+    nc <- ncol(ica$M)
+  }
+  else ica <- ica::icafast(X, nc = nc, center = center)
+  
+  formula <- stats::as.formula(formula)
+  
+  formulas <- lapply(seq_len(nc), function(i) stats::update(formula, paste0("IC", i, " ~ .")))
+  colnames(ica$M) <- paste0("IC", seq_len(nc))
+  p <- cbind(p, ica$M)
+  
+  m <- list()
+  m$model <- lapply(formulas, mgcv::gam, data = p)
+  m$ica <- ica
+  
+  return(m)
+}
+
+#' @import mgcv
+.predict_gam_ica <- function(m, newdata, as.ic = FALSE){
+  preds <- do.call(cbind, lapply(m$model, predict, newdata = newdata))
+  
+  if(!as.ic)
+    return(tcrossprod(m$ica$S, preds))
+  else return(preds)
+}
