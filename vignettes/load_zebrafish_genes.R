2c2559ac8bb28eff8a3573f03f67cbbfbe44dd9c
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2020-05-07 17:46:23 +0200

    Extracted zebrafish genelist loading (#25)

---
+++ vignettes/load_zebrafish_genes.R
@@ -0,0 +1,6 @@
+requireNamespace("biomaRt", quietly = TRUE)
+
+mart <- biomaRt::useMart("ensembl", dataset = "drerio_gene_ensembl")
+zeb_genes <- biomaRt::getBM(attributes = c("ensembl_gene_id", "transcript_length"), 
+                            mart = mart)
+rm(mart)
