dc9be8368ea39cb3bf3a178d26fbfc22f7ffa5b7
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2022-08-24 18:24:07 +0200

    Added plot.geim function

--- R/ge_im.R
+++ R/ge_im.R
@@ -149,3 +149,148 @@ print.geim <- function(x, ...){
   cat("\n\t", ats$f)
   cat("\n---\n")
 }
+
+
+
+#' Plot a GEIM object
+#' 
+#' Plots geim interpolation on components.
+#' 
+#' @param x a `geim` object, as returned by \code{\link{ge_im}}.
+#' @param ref a \code{ref} object, as returned by \code{\link{make_ref}}.
+#' @param ncs which components to plot, defaults to all.
+#' @param n.inter interpolation resolution, as in \code{seq(start, end, length.out = n.inter)}. One of \code{ref}, \code{n.inter}, or \code{by.inter} must be specified.
+#' @param by.inter interpolation resolution, as in \code{seq(start, end, by = by.inter)}. One of \code{ref}, \code{n.inter}, or \code{by.inter} must be specified.
+#' @param cov.levels a named list with potential model covariate levels (e.g batch, strain) to predict as (defaults to first level).
+#' @param t.unit an optional string specifying the time unit and t-zero, e.g "h past egg-laying".
+#' @param col,col.i color for data and interpolation line respectively.
+#' @param show.legend,l.pos whether to show the legend, and its position passed on to \code{\link[graphics]{legend}}
+#' @param show.stats whether to print model fit (deviance explained and relative error) and component (variance explained) statistics on the plot.
+#' @param ... additional arguments passed on to \code{\link[graphics]{plot}}.
+#' 
+#' @return Invisibly returns a table with component variance explained and model fit on component indices (R2, deviance explained, relative error).
+#' 
+#' @export
+#' 
+#' @eval interpol_example()
+#' 
+#' @importFrom graphics legend mtext points plot
+#' 
+plot.geim <- function(x, ref=NULL, ncs=NULL, 
+                      n.inter=NULL, by.inter=NULL, cov.levels=NULL, t.unit=NULL, 
+                      col=NULL, col.i='red', 
+                      show.legend=T, l.pos='topleft', show.stats=T,...){
+  p <- attr(x, "pdata")
+  t.var <- attr(x, "vars")$t.var # time variable
+  tv <- p[, t.var]
+  cvars <- attr(x, "vars")$cvars # covariates
+  dr <- attr(x, "dim_red")
+  nc <- attr(x, "nc")
+  
+  if(is.null(ncs)){
+    ncs <- 1L:nc
+  } else if(any(! ncs%in%(1L:nc))){
+    stop(paste0("ncs must be among components fit by model (1-",nc,"."))
+  }
+  
+  if(!is.null(ref)){ # if ref is supplied
+    if(class(ref)!="ref"){
+      stop("'ref' must be a ref object.")
+    }
+    ts <- ref$time
+    cov.levels <- ifelse(is.null(cov.levels), attr(ref, "cov.level"))
+    t.unit <- ifelse(is.null(t.unit), attr(ref, "t.unit"))
+    
+  } else {
+    if(is.null(n.inter) & is.null(by.inter)){
+      stop("One of ref, n.inter, or by.inter must be specified.")
+    }
+    cov.levels <- .cov_check(x, cov.levels)
+    if(is.null(t.unit)){
+      t.unit <- "no unit specified"
+    }
+    # n/by.inter param handling
+    if(!is.null(by.inter)){
+      if(!is.numeric(by.inter)){
+        stop("by.inter must be a single numeric value.")
+      }
+      ts <- seq(min(tv), max(tv), by = by.inter)
+    } else {
+      ts <- seq(min(tv), max(tv), l = n.inter)
+    }
+    
+  }
+  # make the new predictor dataframe
+  fit_dat <- rep(T, nrow(p)) # keep track of points used for prediction
+  l <- length(ts)
+  ndat <- data.frame(time = ts)
+  colnames(ndat)[1] <- t.var
+  for(v in cvars){
+    ndat[, v] <- factor(rep(cov.levels[[v]], l=l))
+    fit_dat[ (p[,v] != cov.levels[v])] <- F # unused for prediction
+  }
+  
+  if(dr == "pca"){
+    yl <- "PC"
+    y0df <- x[[dr]]$x
+    vexp <- summary(x[[dr]])$importance[2,1L:length(x$model)]
+  } else {
+    yl <- "IC"
+    y0df <- x[[dr]]$M
+    vexp <- x[[dr]]$vafs
+  }
+  user_col <- F
+  if(is.null(col)){
+    col <- c(makeTransparent(par("fg")), par("fg"))[1+fit_dat]
+  } else if(length(col)==1){
+    col <- c(makeTransparent(col), col)[1+fit_dat]
+  } else {
+    warning("User-defined colors: removing elements from legend.")
+    user_col <- T
+  }
+  
+  xl <- paste0("Reference time, ", t.unit)
+  y1df <- predict(x, ndat, as.c=T)
+  
+  m_stats <- do.call(rbind, lapply(x$model, function(m){
+    c(r2 = 1 - sum(m$residuals^2)/sum(m$y**2),
+      deviance.expl = (m$null.deviance - m$deviance)/m$null.deviance,
+      relative.err = sum(abs(m$residuals/m$y))/length(m$y))
+  }))
+  m_stats <- cbind(component.var.exp=vexp, m_stats)
+  rownames(m_stats) <- paste0(yl, 1L:length(x$model))
+  
+  invisible(sapply(ncs, function(i){
+    yli <- ifelse(show.stats, paste0(yl, i, ' (', round(100*m_stats[i, "component.var.exp"], 2) , '%)'), paste0(yl, i))
+    graphics::plot(tv, y0df[,i], 
+         ylab = yli, xlab = xl, 
+         lwd=2, col = col, ...)
+    graphics::points(ts, y1df[,i], type='l', lwd=2, col = col.i)
+    if(i==ncs[1] & show.legend){
+      if(user_col){
+        graphics::legend(l.pos, bty='n', legend=c("data", "model fit"), 
+               lty=c(NA, 1), lwd=c(2,3), 
+               col = c(col[1], col.i),
+               pch=c(1, NA))
+      } else if(all(fit_dat)){
+        graphics::legend(l.pos, bty='n', legend=c("data", "model fit"), 
+               lty=c(NA, 1), lwd=c(2,3), 
+               col = c(col[1], col.i),
+               pch=c(1, NA))
+      } else {
+        graphics::legend(l.pos, bty='n', legend=c("data (all)", "data (selected cov.)", "model fit"), 
+               lty=c(NA, NA,1), lwd=c(2,2,3), col = c(unique(col), col.i),
+               pch=c(1, 1, NA))
+      }
+    }
+    if(show.stats){
+      graphics::mtext(paste0("DE = ", round(m_stats[i,"deviance.expl"], 4)), side = 3, adj = 0,
+            at=par("usr")[1], line = 0, cex = .75)
+      graphics::mtext(paste0("RE = ", round(m_stats[i,"relative.err"], 4)), side = 3, adj = 1,
+            at=par("usr")[2], line = 0, cex = .75)
+    }
+    
+  }))
+  
+  return(invisible(m_stats))
+}
