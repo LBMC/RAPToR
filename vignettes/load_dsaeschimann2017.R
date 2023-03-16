256fc8ae8dd166250a36ba86855b59f990a5d02b
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2023-03-16 10:58:54 +0100

    Formatted code to 80char per line (#42)

--- vignettes/load_dsaeschimann2017.R
+++ vignettes/load_dsaeschimann2017.R
@@ -1,19 +1,28 @@
 geo_dsaeschimann2017 <- "GSE80157"
 
-g_url_dsaeschimann2017 <- GEOquery::getGEOSuppFiles(geo_dsaeschimann2017, makeDirectory = FALSE, fetch_files = FALSE)
+g_url_dsaeschimann2017 <- GEOquery::getGEOSuppFiles(geo_dsaeschimann2017, 
+                                                    makeDirectory = FALSE, 
+                                                    fetch_files = FALSE)
 g_file_dsaeschimann2017 <- paste0(data_folder, "dsaeschimann2017.txt.gz")
-utils::download.file(url = as.character(g_url_dsaeschimann2017$url[2]), destfile = g_file_dsaeschimann2017)
+utils::download.file(url = as.character(g_url_dsaeschimann2017$url[2]), 
+                     destfile = g_file_dsaeschimann2017)
 
-X_dsaeschimann2017 <- read.table(gzfile(g_file_dsaeschimann2017), h=T, sep = '\t', stringsAsFactors = F, row.names = 1)
+X_dsaeschimann2017 <- read.table(gzfile(g_file_dsaeschimann2017), 
+                                 h=T, sep = '\t', stringsAsFactors = F, 
+                                 row.names = 1)
 
 # convert to tpm & wb_id
-X_dsaeschimann2017 <- X_dsaeschimann2017[rownames(X_dsaeschimann2017)%in%wormRef::Cel_genes$wb_id,]
-X_dsaeschimann2017 <- raw2tpm(rawcounts = X_dsaeschimann2017, 
-                              genelengths = wormRef::Cel_genes$transcript_length[match(rownames(X_dsaeschimann2017),
-                                                                                       wormRef::Cel_genes$wb_id)])
+X_dsaeschimann2017 <- X_dsaeschimann2017[
+  rownames(X_dsaeschimann2017) %in% wormRef::Cel_genes$wb_id,]
+
+X_dsaeschimann2017 <- raw2tpm(
+  rawcounts = X_dsaeschimann2017, 
+  genelengths = wormRef::Cel_genes$transcript_length[
+    match(rownames(X_dsaeschimann2017), wormRef::Cel_genes$wb_id)])
 
 # pheno data
-P_dsaeschimann2017 <- Biobase::pData(GEOquery::getGEO(geo_dsaeschimann2017, getGPL = F)[[1]])
+P_dsaeschimann2017 <- Biobase::pData(GEOquery::getGEO(geo_dsaeschimann2017, 
+                                                      getGPL = F)[[1]])
 P_dsaeschimann2017[,10:34] <- NULL
 P_dsaeschimann2017[, 3:8] <- NULL
 
@@ -21,18 +30,24 @@ colnames(P_dsaeschimann2017)[4] <- "strain"
 P_dsaeschimann2017$strain <- factor(P_dsaeschimann2017$strain)
 P_dsaeschimann2017$title <- make.names(P_dsaeschimann2017$title)
 
-colnames(X_dsaeschimann2017) <- gsub('RNASeq_riboM_', '', colnames(X_dsaeschimann2017), fixed = T)
-P_dsaeschimann2017$title <- gsub('RNASeq_riboM_', '', P_dsaeschimann2017$title, fixed = T)
+# get age from sample name
+P_dsaeschimann2017$age <- as.numeric(
+  sub('(\\d+)\\shours', '\\1', P_dsaeschimann2017$`time in development:ch1`))
 
-# get age 
-P_dsaeschimann2017$age <- as.numeric(sub('(\\d+)\\shours', '\\1', P_dsaeschimann2017$`time in development:ch1`))
-
-
+# formatting
+colnames(X_dsaeschimann2017) <- gsub('RNASeq_riboM_', '', 
+                                     colnames(X_dsaeschimann2017), fixed = T)
+P_dsaeschimann2017$title <- gsub('RNASeq_riboM_', '', 
+                                 P_dsaeschimann2017$title, fixed = T)
 X_dsaeschimann2017 <- X_dsaeschimann2017[, P_dsaeschimann2017$title]
 
+# save data
 dsaeschimann2017 <- list(g = X_dsaeschimann2017, p = P_dsaeschimann2017)
-save(dsaeschimann2017, file = paste0(data_folder, "dsaeschimann2017.RData"), compress = "xz")
+
+save(dsaeschimann2017, file = paste0(data_folder, "dsaeschimann2017.RData"), 
+     compress = "xz")
 
 # cleanup
 file.remove(g_file_dsaeschimann2017)
-rm(geo_dsaeschimann2017, g_url_dsaeschimann2017, g_file_dsaeschimann2017, X_dsaeschimann2017, P_dsaeschimann2017)
+rm(geo_dsaeschimann2017, g_url_dsaeschimann2017, g_file_dsaeschimann2017, 
+   X_dsaeschimann2017, P_dsaeschimann2017)
--- vignettes/load_dshendriks2014.R
+++ vignettes/load_dshendriks2014.R
@@ -1,39 +1,57 @@
 geo_dshendriks2014 <- "GSE52861"
 
-g_url_dshendriks2014 <- GEOquery::getGEOSuppFiles(geo_dshendriks2014, makeDirectory = FALSE, fetch_files = FALSE)
+g_url_dshendriks2014 <- GEOquery::getGEOSuppFiles(geo_dshendriks2014, 
+                                                  makeDirectory = FALSE, 
+                                                  fetch_files = FALSE)
 g_file_dshendriks2014 <- paste0(data_folder, "dshendriks2014.txt.gz")
-utils::download.file(url = as.character(g_url_dshendriks2014$url[2]), destfile = g_file_dshendriks2014)
+utils::download.file(url = as.character(g_url_dshendriks2014$url[2]), 
+                     destfile = g_file_dshendriks2014)
 
-X_dshendriks2014 <- read.table(gzfile(g_file_dshendriks2014), h=T, sep = '\t', stringsAsFactors = F, row.names = 1)
+X_dshendriks2014 <- read.table(gzfile(g_file_dshendriks2014), 
+                               h=T, sep = '\t', stringsAsFactors = F, 
+                               row.names = 1)
 
 # convert to tpm & wb_id
-X_dshendriks2014 <- X_dshendriks2014[rownames(X_dshendriks2014)%in%wormRef::Cel_genes$wb_id,]
-X_dshendriks2014 <- raw2tpm(rawcounts = X_dshendriks2014, 
-                            genelengths = wormRef::Cel_genes$transcript_length[match(rownames(X_dshendriks2014),
-                                                                                     wormRef::Cel_genes$wb_id)])
+X_dshendriks2014 <- X_dshendriks2014[
+  rownames(X_dshendriks2014)%in%wormRef::Cel_genes$wb_id,]
+X_dshendriks2014 <- raw2tpm(
+  rawcounts = X_dshendriks2014, 
+  genelengths = wormRef::Cel_genes$transcript_length[
+    match(rownames(X_dshendriks2014), wormRef::Cel_genes$wb_id)])
 
 
 # pheno data
-P_dshendriks2014 <- Biobase::pData(GEOquery::getGEO(geo_dshendriks2014, getGPL = F)[[1]])
+P_dshendriks2014 <- Biobase::pData(GEOquery::getGEO(geo_dshendriks2014, 
+                                                    getGPL = F)[[1]])
 
 # filter relevant fields/samples
-P_dshendriks2014 <- P_dshendriks2014[(P_dshendriks2014$`strain:ch1` == 'N2') & (P_dshendriks2014$`growth protocol:ch1` == 'Continuous'), ]
-P_dshendriks2014 <- P_dshendriks2014[, c("title", "geo_accession", "time in development:ch1")]
+P_dshendriks2014 <- P_dshendriks2014[
+  (P_dshendriks2014$`strain:ch1` == 'N2') & 
+  (P_dshendriks2014$`growth protocol:ch1` == 'Continuous'), ]
 
-# get age 
-P_dshendriks2014$age <- as.numeric(sub('(\\d+)\\shours', '\\1', P_dshendriks2014$`time in development:ch1`))
+P_dshendriks2014 <- P_dshendriks2014[, c("title", "geo_accession", 
+                                         "time in development:ch1")]
+
+# get age from sample name
+P_dshendriks2014$age <- as.numeric(
+  sub('(\\d+)\\shours', '\\1', P_dshendriks2014$`time in development:ch1`))
 
 
 # formatting
 P_dshendriks2014$title <- gsub('RNASeq_polyA_', '', 
                                gsub('hr', 'h', 
-                                    gsub('-', '.', fixed = T, as.character(P_dshendriks2014$title))))
-colnames(X_dshendriks2014) <- gsub('RNASeq_polyA_','', colnames(X_dshendriks2014))
+                                    gsub('-', '.', fixed = T, 
+                                         as.character(P_dshendriks2014$title))))
+colnames(X_dshendriks2014) <- gsub('RNASeq_polyA_','', 
+                                   colnames(X_dshendriks2014))
 X_dshendriks2014 <- X_dshendriks2014[, P_dshendriks2014$title]
 
+# save data
 dshendriks2014 <- list(g = X_dshendriks2014, p = P_dshendriks2014)
-save(dshendriks2014, file = paste0(data_folder, "dshendriks2014.RData"), compress = "xz")
+save(dshendriks2014, file = paste0(data_folder, "dshendriks2014.RData"), 
+     compress = "xz")
 
 # cleanup
 file.remove(g_file_dshendriks2014)
-rm(geo_dshendriks2014, g_url_dshendriks2014, g_file_dshendriks2014, X_dshendriks2014, P_dshendriks2014)
+rm(geo_dshendriks2014, g_url_dshendriks2014, g_file_dshendriks2014, 
+   X_dshendriks2014, P_dshendriks2014)
