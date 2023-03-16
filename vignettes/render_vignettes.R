041a58542579db463786db135e212c0661be728b
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2023-03-16 16:08:51 +0100

    Updated datapkg vignette with biocstyle (#42)

--- vignettes/render_vignettes.R
+++ vignettes/render_vignettes.R
@@ -1,10 +1,17 @@
 # setwd('vignettes/')
 
-# DE correction vignette
-rmarkdown::render(input = 'RAPToR-DEcorrection.Rmd', output_format = "all", clean = TRUE)
+
+
+# Main vignette
+rmarkdown::render(input = 'RAPToR.Rmd', output_format = "all", clean = TRUE)
 
 # Showcase vignette
 rmarkdown::render(input = 'RAPToR-showcase.Rmd', output_format = "all", clean = TRUE)
 
-# Main vignette
-rmarkdown::render(input = 'RAPToR.Rmd', output_format = "all", clean = TRUE)
+# Data pkg vignette 
+rmarkdown::render(input = 'RAPToR-datapkgs.Rmd', output_format = "all", clean = TRUE)
+
+# DE correction vignette
+rmarkdown::render(input = 'RAPToR-DEcorrection.Rmd', output_format = "all", clean = TRUE)
+
+
