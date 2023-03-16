549c31ca87542d19944eba2748a36e227db69071
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2023-03-16 11:00:44 +0100

    Added main vignette to list (#42)

--- vignettes/render_vignettes.R
+++ vignettes/render_vignettes.R
@@ -5,3 +5,6 @@ rmarkdown::render(input = 'RAPToR-DEcorrection.Rmd', output_format = "all", clea
 
 # Showcase vignette
 rmarkdown::render(input = 'RAPToR-showcase.Rmd', output_format = "all", clean = TRUE)
+
+# Main vignette
+rmarkdown::render(input = 'RAPToR.Rmd', output_format = "all", clean = TRUE)
