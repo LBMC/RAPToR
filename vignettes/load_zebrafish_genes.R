2e13cced9f42adfc16042baadf057e8367e82e8b
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2023-03-24 17:19:43 +0100

    Formatted code to 80char per line (#42)

--- vignettes/load_zebrafish_genes.R
+++ vignettes/load_zebrafish_genes.R
@@ -1,6 +1,7 @@
 requireNamespace("biomaRt", quietly = TRUE)
 
 mart <- biomaRt::useMart("ensembl", dataset = "drerio_gene_ensembl")
-zeb_genes <- biomaRt::getBM(attributes = c("ensembl_gene_id", "transcript_length"), 
+zeb_genes <- biomaRt::getBM(attributes = c("ensembl_gene_id", 
+                                           "transcript_length"), 
                             mart = mart)
 rm(mart)
