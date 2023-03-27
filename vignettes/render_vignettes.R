087b0f0a9affbb2be6e1057bdf9ae7bb53118826
Romain Bulteau <romain.bulteau@ens-lyon.fr>
2023-03-27 13:06:01 +0200

    managed vignette pdf output for separate index entry (#42)

--- vignettes/render_vignettes.R
+++ vignettes/render_vignettes.R
@@ -17,3 +17,24 @@ rmarkdown::render(input = 'RAPToR-DEcorrection.Rmd', output_format = "all", clea
 # Refbuilding vignette
 rmarkdown::render(input = 'RAPToR-refbuilding.Rmd', output_format = "all", clean = TRUE)
 
+
+
+vlist <- c("RAPToR",
+           "RAPToR-showcase",
+           "RAPToR-datapkgs",
+           "RAPToR-DEcorrection",
+           "RAPToR-refbuilding")
+
+# rename the pdf output for separate vignette index entry
+file.rename(from =  paste0(vlist, ".pdf"), to =  paste0(vlist, "-pdf.pdf"))
+
+# create vignette .asis files for html and pdf entries
+for(v in vlist){
+  vpdf <- paste0(v, "-pdf")
+  txts <- paste0(
+    "%\\VignetteIndexEntry{",c(v, vpdf),"}\n",
+    "%\\VignetteEngine{R.rsp::asis}\n")
+
+  cat(txts[1], file = paste0(v, ".html.asis"), append = F)
+  cat(txts[2], file = paste0(vpdf, ".pdf.asis"), append = F)
+}
